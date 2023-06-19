fe745bf25ec633624075210e07b5e54f041d765a
Fahyvor <favourokafor30@gmail.com>
2023-06-19 11:34:57 +0100

    Store api

---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,51 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({})
+    .sort('name')
+    .select('name, price')
+    res.status(200).json({ products, nbHits: products.length });
+}
+
+const getAllProducts = async (req, res) => {
+    const { featured, company, name, sort, fields } = req.query;
+    const queryObject = {}
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if(company) {
+        queryObject.company = company
+    }
+    if(name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    // console.log(queryObject);
+    let result = Product.find(queryObject);
+
+    // sort
+    if(sort) {
+        const sortList = sort.split(',').join('')
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createAt')
+    }
+
+    if (fields) {
+        const fieldList = fields.split(',').join('')
+        result = result.select(fieldList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit)
+    // 23 
+    // 4 7 7 7 2
+    const products = await result
+    res.status(200).json({ products, nbHits: products.length });
+}
+
+
+module.exports = {getAllProductsStatic, getAllProducts}
