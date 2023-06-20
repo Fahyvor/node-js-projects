6554ece3449ac7cc100b8286110b4dd0a5fb0ff8
Fahyvor <favourokafor30@gmail.com>
2023-06-20 15:19:12 +0100

    JWT Basics

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,10 +1,46 @@
+//check username, password in post(login) request
+// if exist create new JWT
 
+// setup authentication so only the request with JWT can access the dashboard
+
+const CustomeAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
-    res.send('Fake Login/Register/Signup')
+    const {username, password} = req.body
+    console.log(username, password);
+    // mongo
+    // Joi
+    // check in the controller
+
+    if(!username || !password) {
+        throw new CustomeAPIError('Please provide email and password', 400)
+    }
+
+    // just for demo, normally provided by DB
+    const id = new Date().getDate()
+
+    // try to keep payload small, better experience for user
+    // just for demo, in production use long, complex and unguessable string value !!!!
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d',})
+
+    res.status(200).json({msg: 'user created', token});
 }
 
 const dashboard = async (req,res) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomeAPIError('No token provided', 401)
+    }
+
+    const token = authHeader.split('')[1]
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded)
+    } catch (error) {
+        throw new CustomeAPIError('Not authorised to access this route', 401)
+    }
+
     const luckyNumber = Math.floor(Math.random()*100);
     res.status(200).json({msg: `Hello, Elrey Favour`, secret: `Here is your authorized data,
     your lucky number is ${luckyNumber}`,})
